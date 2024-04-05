6cd9fb9d81dfe2f8be4821052be67378e50bebac
oussema <oussemachaieb7@gmail.com>
2024-04-05 12:51:23 +0100

    Update UI

--- config.lua
+++ config.lua
@@ -1,122 +1,172 @@
 Config = {}
 
-Config.Cars = {
-    one = {
-        categories = {
-            { 
-                name = "Cycles",
-                cars = {
-                    { name = "bmx", price = 7 }
-                }
-            },
-            { 
-                name = "Compacts",
-                cars = {
-                    { name = "serrano", price = 150 },
-                    { name = "brioso2", price = 150 },
-                    { name = "blista", price = 110 },
-                }
-            },
-            { 
-                name = "Suvs",
-                cars = {
-                    { name = "baller2", price = 200 },
-                    { name = "seminole2", price = 165 }
-                }
-            },
-            { 
-                name = "Muscle",
-                cars = {
-                    { name = "deviant", price = 220 }
-                }
-            },
-            { 
-                name = "Sports",
-                cars = {
-                    { name = "carbonizzare", price = 180 }
-                }
-            },
-            { 
-                name = "Coupes",
-                cars = {
-                    { name = "futo", price = 185 }
-                }
-            },
-            { 
-                name = "MotorCycles",
-                cars = {
-                    { name = "faggio2", price = 35 }
-                }
-            },
-            { 
-                name = "Sedans",
-                cars = {
-                    { name = "surge", price = 150 }
-                }
-            },
+Config.Rent = {
+    Category = {
+        {name = "Compacts"},
+        {name = "Sedans"},
+        {name = "SUVs"},
+        {name = "Muscle"},
+        {name = "Sports"},
+        {name = "Super"},
+    },
+
+    Cars = {
+   
+        ["Compacts"]  = {
+            {name = "Blista", price = 350 , model = "blista" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/blista.png"},
+            {name = "Brioso R/A", price = 450 , model = "brioso" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/brioso.png"},
+            {name = "Dilettante", price = 250 , model = "dilettante" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/dilettante.png"},
+            {name = "Issi", price = 150 , model = "issi2" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/issi2.png"},
+            {name = "Panto", price = 750 , model = "panto" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/panto.png"},
+            {name = "Prairie", price = 630 , model = "prairie" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/prairie.png"},
+            {name = "Rhapsody", price = 420 , model = "rhapsody" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/rhapsody.png"},
+        },
+        ["Sedans"] = {
+            {name = "Asea", price = 852 , model = "asea" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/asea.png"},
+            {name = "Asterope", price = 430 , model = "asterope" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/asterope.png"},
+            {name = "Cog55", price = 658 , model = "cog55" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/cog55.png"},
+            {name = "Cognoscenti", price = 750 , model = "cognoscenti" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/cognoscenti.png"},
+            {name = "Cognoscenti55", price = 1010 , model = "cognoscenti2" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/cognoscenti2.png"},
+            {name = "Emperor", price = 2240 , model = "emperor" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/emperor.png"},
+            {name = "Fugitive", price = 4210 , model = "fugitive" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/fugitive.png"},
+            {name = "Glendale", price = 332 , model = "glendale" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/glendale.png"},
+            {name = "Ingot", price = 445 , model = "ingot" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/ingot.png"},
+            {name = "Intruder", price = 665 , model = "intruder" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/intruder.png"},
+            {name = "Premier", price = 445 , model = "premier" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/premier.png"},
+        },
+        ["SUVs"] = {
+            {name = "Baller", price = 225 , model = "baller" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/baller.png"},
+            {name = "Baller2", price = 552 , model = "baller2" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/baller2.png"},
+            {name = "Baller3", price = 663 , model = "baller3" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/baller3.png"},
+            {name = "Baller4", price = 336 , model = "baller4" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/baller4.png"},
+            {name = "Baller5", price = 448 , model = "baller5" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/baller5.png"},
+            {name = "Baller6", price = 500 , model = "baller6" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/baller6.png"},
+            {name = "Cavalcade", price = 300 , model = "cavalcade" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/cavalcade.png"},
+            {name = "Cavalcade2", price = 200 , model = "cavalcade2" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/cavalcade2.png"},
+            {name = "Dubsta", price = 100 , model = "dubsta" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/dubsta.png"},
+            {name = "Dubsta2", price = 541 , model = "dubsta2" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/dubsta2.png"},
+            {name = "Fq2", price = 147 , model = "fq2" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/fq2.png"},
+        },
+        ["Muscle"] = {
+            {name = "Blade", price = 1000 , model = "blade" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/blade.png"},
+            {name = "Buccaneer", price = 1000 , model = "buccaneer" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/buccaneer.png"},
+            {name = "Buccaneer2", price = 1000 , model = "buccaneer2" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/buccaneer2.png"},
+            {name = "Chino", price = 1000 , model = "chino" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/chino.png"},
+            {name = "Chino2", price = 1000 , model = "chino2" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/chino2.png"},
+            {name = "Coquette3", price = 1000 , model = "coquette3" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/coquette3.png"},
+            {name = "Dominator", price = 1000 , model = "dominator" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/dominator.png"},
+            {name = "Dominator2", price = 1000 , model = "dominator2" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/dominator2.png"},
+            {name = "Dukes", price = 1000 , model = "dukes" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/dukes.png"},
+            {name = "Dukes2", price = 1000 , model = "dukes2" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/dukes2.png"},
+            {name = "Faction", price = 1000 , model = "faction" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/faction.png"},
+        },
+        ["Sports"] = {
+            {name = "Alpha", price = 1000 , model = "alpha" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/alpha.png"},
+            {name = "Banshee", price = 1000 , model = "banshee" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/banshee.png"},
+            {name = "BestiaGTS", price = 1000 , model = "bestiagts" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/bestiagts.png"},
+            {name = "Buffalo", price = 1000 , model = "buffalo" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/buffalo.png"},
+            {name = "Buffalo2", price = 1000 , model = "buffalo2" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/buffalo2.png"},
+            {name = "Buffalo3", price = 1000 , model = "buffalo3" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/buffalo3.png"},
+            {name = "Carbonizzare", price = 1000 , model = "carbonizzare" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/carbonizzare.png"},
+            {name = "Comet2", price = 1000 , model = "comet2" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/comet2.png"},
+            {name = "Comet3", price = 1000 , model = "comet3" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/comet3.png"},
+            {name = "Comet4", price = 1000 , model = "comet4" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/comet4.png"},
+            {name = "Comet5", price = 1000 , model = "comet5" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/comet5.png"},
+        },
+        ["Super"] = {
+            {name = "Adder", price = 1000 , model = "adder" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/adder.png"},
+            {name = "Autarch", price = 1000 , model = "autarch" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/autarch.png"},
+            {name = "Banshee2", price = 1000 , model = "banshee2" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/banshee2.png"},
+            {name = "Bullet", price = 1000 , model = "bullet" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/bullet.png"},
+            {name = "Cheetah", price = 1000 , model = "cheetah" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/cheetah.png"},
+            {name = "Cyclone", price = 1000 , model = "cyclone" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/cyclone.png"},
+            {name = "EntityXF", price = 1000 , model = "entityxf" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/entityxf.png"},
+            {name = "FMJ", price = 1000 , model = "fmj" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/fmj.png"},
+            {name = "GP1", price = 1000 , model = "gp1" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/gp1.png"},
+            {name = "Infernus", price = 1000 , model = "infernus" , img = "https://raw.githubusercontent.com/MericcaN41/gta5carimages/main/images/infernus.png"}, 
+        },
+    },
+}
+
+Config.Boat = {
+    Category = {
+        {name = "Boats"}
+    },
+
+    Cars = {
+        ["Boats"]  = {
+            {name = "Sea Shark", price = 350 , model = "seashark" , img = "https://r2.fivemanage.com/ZrUYLRK1lGQX9xgdwjNb3/seaashark.png"},
+            {name = "Dinghy", price = 450 , model = "dinghy" , img = "https://r2.fivemanage.com/ZrUYLRK1lGQX9xgdwjNb3/dinghyy.png"},
+            {name = "Jetmax", price = 250 , model = "jetmax" , img = "https://r2.fivemanage.com/ZrUYLRK1lGQX9xgdwjNb3/jetmaxx.png"},
+            {name = "Marquis", price = 150 , model = "marquis" , img = "https://r2.fivemanage.com/ZrUYLRK1lGQX9xgdwjNb3/marquiis.png"},
+            {name = "Speeder", price = 750 , model = "speeder" , img = "https://r2.fivemanage.com/ZrUYLRK1lGQX9xgdwjNb3/speederr.png"},
+            {name = "Toro", price = 630 , model = "toro" , img = "https://r2.fivemanage.com/ZrUYLRK1lGQX9xgdwjNb3/toroo.png"},
+            {name = "Long fin", price = 420 , model = "longfin" , img = "https://r2.fivemanage.com/ZrUYLRK1lGQX9xgdwjNb3/longfin.png"},
         }
     },
 }
 
 Config.Locations = {
-    ['one'] = {
-        ['label'] = 'Rent you',
+    [1] = {
+        ['label'] = 'Car Rental',
         ['coords'] = vector4(-291.89, -985.96, 31.08, 340.31),
         ['carspawn'] = vector4(-297.76, -989.99, 30.49, 339.02),
+        ['categorie'] = Config.Rent,
         ['ped'] = 'a_m_m_hasjew_01',
         ['scenario'] = 'WORLD_HUMAN_STAND_MOBILE',
         ['radius'] = 1.5,
-        ['targetIcon'] = 'fas fa-shopping-basket',
+        ['targetIcon'] = 'fas fa-car',
         ['targetLabel'] = 'Rent Vehicle',
-        ["category"] = Config.Cars.one,
         ['showblip'] = true,
         ['blipsprite'] = 38,
         ['blipscale'] = 0.6,
         ['blipcolor'] = 38
     },
-    ['two'] = {
-        ['label'] = 'Rent you',
+    [2] = {
+        ['label'] = 'Car Rental',
         ['coords'] = vector4(1852.37, 2582.12, 45.67, 270.72),
         ['carspawn'] = vector4(1855.2, 2578.72, 45.08, 271.21),
+        ['categorie'] = Config.Rent,
         ['ped'] = 'a_m_m_hasjew_01',
         ['scenario'] = 'WORLD_HUMAN_STAND_MOBILE',
         ['radius'] = 1.5,
-        ['targetIcon'] = 'fas fa-shopping-basket',
+        ['targetIcon'] = 'fas fa-car',
         ['targetLabel'] = 'Rent Vehicle',
-        ["category"] = Config.Cars.one,
         ['showblip'] = true,
         ['blipsprite'] = 38,
         ['blipscale'] = 0.6,
         ['blipcolor'] = 38
     },
-    ['three'] = {
-        ['label'] = 'Rent you',
+    [3] = {
+        ['label'] = 'Car Rental',
         ['coords'] = vector4(281.66, -1360.31, 31.92, 52.89),
         ['carspawn'] = vector4(281.64, -1353.49, 31.34, 141.33),
+        ['categorie'] = Config.Rent,
         ['ped'] = 'a_m_m_hasjew_01',
         ['scenario'] = 'WORLD_HUMAN_STAND_MOBILE',
         ['radius'] = 1.5,
-        ['targetIcon'] = 'fas fa-shopping-basket',
+        ['targetIcon'] = 'fas fa-car',
         ['targetLabel'] = 'Rent Vehicle',
-        ["category"] = Config.Cars.one,
         ['showblip'] = true,
         ['blipsprite'] = 38,
         ['blipscale'] = 0.6,
         ['blipcolor'] = 38
     },
-    ['four'] = {
-        ['label'] = 'Rent you',
-        ['coords'] = vector4(-679.7, -1112.75, 14.67, 35.1),
-        ['carspawn'] = vector4(-683.37, -1112.37, 13.93, 32.06),
+    [4] = {
+        ['label'] = 'Boat Rental',
+        ['coords'] = vector4(-806.81, -1497.1, 1.6, 114.24),
+        ['carspawn'] = vector4(-806.59, -1504.73, -0.09, 124.98),
+        ['categorie'] = Config.Boat,
         ['ped'] = 'a_m_m_hasjew_01',
         ['scenario'] = 'WORLD_HUMAN_STAND_MOBILE',
         ['radius'] = 1.5,
-        ['targetIcon'] = 'fas fa-shopping-basket',
-        ['targetLabel'] = 'Rent Vehicle',
-        ["category"] = Config.Cars.one,
+        ['targetIcon'] = 'fas fa-boat',
+        ['targetLabel'] = 'Rent Boat',
         ['showblip'] = true,
         ['blipsprite'] = 38,
         ['blipscale'] = 0.6,
         ['blipcolor'] = 38
     },
 }
+
+Config.Fuel = 'cdn-fuel'
